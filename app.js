5b5532fe7cef7d6ff67c550064eb31668f5faf73
Ihar Amelyaniuk <Ihar_Amelyaniuk@epam.com>
2018-02-14 17:59:20 +0300

    set up enviroment

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const winston = require('winston');
-const { combine, timestamp, printf } = winston.format;
 const mongoose = require('mongoose');
 const config = require('./config/database');
-
-const myFormat = printf(info => `${info.timestamp} - ${info.message}`);
-const logger = winston.createLogger({
-    level: 'info',
-    format: combine(
-        timestamp(),
-        myFormat
-    ),
-    transports: [
-        new winston.transports.File({filename: 'history.log'})
-    ]
-});
 
 mongoose.connect(config.database);
 
@@ -26,21 +12,13 @@ app.set('views', 'views');
 app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    logger.info(req.method + ' ' + req.url);
-    next();
-});
 app.use('/blogs', blogs);
 
 app.use((err, req, res, next) => {
     res.status(500).json(err);
 });
 
-app.get('/', (req, res) => res.status(200).render('blogs/blogs'));
-
-app.get('*', (req, res) => {
-    res.render('404/404', { title: 'Page is not found!' });
-});
+app.get('/', (req, res) => res.status(200).send('Hello'));
 
 const PORT = 4100;
 app.listen(PORT, function () {
---
+++ client/src/containers/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './app.less'
+
+const App = () => {
+    return (
+        <div className="main">1111213</div>
+    )
+}
+
+export default App
